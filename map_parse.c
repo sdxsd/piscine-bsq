87c644f0d7b97e0eef12985e1f1aafac0c1a6db3
charlottejaeger8@gmail.com <cjaeger@f1r4s12.codam.nl>
2021-07-26 11:44:44 +0200

    map_parse update

--- map_parse.c
+++ map_parse.c
@@ -1,4 +1,53 @@
-int **map_parse(char *map)
+int *dimensions(char *map)
 {
-   // Code this shit.
+   int index;
+   int i;
+   int j;
+
+   index = 0;
+   i = 0;
+   j = 0;
+
+   while(map[index] != '\n')
+	  i++;
+
+   index = 0;
+
+   while(map[index] != '\0')
+   {
+	  if(map[index] == '\n')
+		 j++;
+   }
+   dimensions[1] = i;
+   dimensions[2] = j;
+   return(dimensions)
+}
+
+char **map_parse(char *map, int dimensions)
+{
+   int i;
+   int j;
+   int count_i;
+   int count_j;
+   int index;
+
+   i = dimensions[1];
+   j = dimensions[2];
+   count_i = 0;
+   count_j = 0;
+   index = 0;
+
+   while(count_j < j)
+   {
+	  while(count_i < i)
+	  {
+		 map_parse[i][j] = map[index];
+		 index++;
+		 i++;
+	  }
+	  j++;
+	  i = 0;
+   }
+   return(map_parse);
+
 }
